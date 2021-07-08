5296b5f44266c19d83a35befffd4affb27ab9ced
Zwt1 <62884390+Zwt1@users.noreply.github.com>
2021-07-08 16:05:37 +0800

    Update constants.js

--- zjvis/webapp/utils/constants.js
+++ zjvis/webapp/utils/constants.js
@@ -1,10 +1,17 @@
-/*
- * @Author: your name
- * @Date: 2021-03-30 10:18:06
- * @LastEditTime: 2021-04-02 18:18:29
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \frontend\src\utils\constants.js
+/** Copyright 2020 Tianshu AI Platform. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================
  */
 const HOSTNAME = window.location.origin + '/'
 const DJANGOHOSTNAME = 'http://localhost:8013/'
