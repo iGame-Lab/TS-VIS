512b8b5b465e9aa3fd8b82b0ed534946661b6f3b
Feyily <630924780@qq.com>
2021-08-31 16:19:46 +0800

    chore: Fix some typos in README.md and update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 # -*- coding: UTF-8 -*-
+"""
+ Copyright 2021 Tianshu AI Platform. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ =============================================================
+"""
+
 import os
 import sys
 import subprocess
 import shutil
 from setuptools import setup, find_packages
 
-# 移除构建的build文件夹
-CUR_PATH = os.path.dirname(os.path.abspath(__file__))
 VERSION = "0.4.2"
+
+CUR_PATH = os.path.dirname(os.path.abspath(__file__))
 ROOT = os.path.realpath(os.path.dirname(__file__))
 preparing_PyPI_package = 'sdist' in sys.argv or 'bdist_wheel' in sys.argv
 
@@ -39,18 +56,21 @@ def write_version():
 
 
 INSTALL_REQUIRES = read("requirements.txt")
+README = read('README.md')
 write_version()
 setup(
-    name='tsvis',
+    name='TS-VIS',
     version=VERSION,
-    author='hdu',
+    author='iGame',
     author_email='',
+    long_description=README,
+    long_description_content_type='text/markdown',
     # url='',
-    # license = '',
-    description="Visualize the training process of the neural network",
+    license = 'Apache License',
+    description="TS-VIS is a Python module for deep learning visualization",
     packages=find_packages(),
     include_package_data=True,  # 启用清单文件MANIFEST.in,包含数据文件
-    entry_points={'console_scripts': ['tsvis = tsvis.server.main:run'] },  # 动态法线服务和插件
+    entry_points={'console_scripts': ['tsvis = tsvis.server.main:run'] },
     install_requires=INSTALL_REQUIRES,
 )
 if not preparing_PyPI_package:
